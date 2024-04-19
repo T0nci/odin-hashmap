6194dc3325caf33950879a20c5bb8aa2640c1a67
T0nci <138928016+T0nci@users.noreply.github.com>
2024-04-19 22:14:42 +0200

    Add functions for get, set and has

--- hashmap.js
+++ hashmap.js
@@ -1,17 +1,15 @@
-class Node {
-  constructor(key, value) {
+function Node(key, value) {
     if (!key || !value) throw new Error("Node must have a key and value!");
 
-    this.key = key;
-    this.value = value;
-    this.nextNode = null;
-  }
+    return {key, value, nextNode: null};
 }
 
 function HashMap() {
   const hashMap = [];
-  const capacity = 16;
+  let capacity = 16;
   const LOAD_FACTOR = 0.75;
+
+  for (let i = 0; i < capacity; i += 1) hashMap[i] = null;
 
   function hash(key) {
     let hashCode = 0;
@@ -24,5 +22,91 @@ function HashMap() {
     return hashCode;
   }
 
-  return {};
+  function set(key, value) {
+    // if ((length() + 1) / capacity > LOAD_FACTOR) {
+    //   regrow();
+    // }
+    
+    const hashedKey = hash(key);
+
+    if (hashMap[hashedKey] === null) {
+      hashMap[hashedKey] = Node(key, value);
+      return;
+    }
+
+    let node = hashMap[hashedKey];
+    while (node.nextNode !== null) {
+      if (node.key === key) {
+        node.value = value;
+        return;
+      }
+
+      node = node.nextNode;
+    }
+
+    if (node.key === key) {
+      node.value = value;
+      return;
+    }
+
+    node.nextNode = Node(key, value);
+  }
+
+  function get(key) {
+    const hashedKey = hash(key);
+
+    if (hashMap[hashedKey] === null) {
+      return null;
+    }
+
+    let node = hashMap[hashedKey];
+    while (node.nextNode !== null) {
+      if (node.key === key) {
+        return node.value;
+      }
+
+      node = node.nextNode;
+    }
+
+    if (node.key === key) {
+      return node.value;
+    }
+
+    return null;
+  }
+
+  function has(key) {
+    const hashedKey = hash(key);
+
+    if (hashMap[hashedKey] === null) {
+      return false;
+    }
+
+    let node = hashMap[hashedKey];
+    while (node.nextNode !== null) {
+      if (node.key === key) {
+        return true;
+      }
+
+      node = node.nextNode;
+    }
+
+    if (node.key === key) {
+      return true;
+    }
+
+    return false;
+  }
+
+  return {
+    set,
+    get,
+    has,
+  };
 }
+
+const hashMap = HashMap();
+hashMap.set('White', 'Shark');
+
+console.log(hashMap.get('Shark'));
+console.log(hashMap.get('White'));
